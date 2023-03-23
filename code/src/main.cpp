7ed596445af5fe31900e7f10d6af307293b11a7e
rmingon <ronanmingon69550@gmail.com>
2023-03-23 16:19:24 +0100

    add lidar pin

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,23 +1,33 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include <Adafruit_PWMServoDriver.h>
 #include <Servo.h>
 #include <Led.h>
 
 Led led1(32);
+Led led2(33);
 
-#define SERVO_FREQ 50
+int lidar_motor = 26;
+int lidar_motor_speed = 37;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("8 channel Servo test!");
 
+Serial1.begin(128000);
+
+
   servoInit();
+
+  pinMode(lidar_motor, OUTPUT);
+  digitalWrite(lidar_motor, LOW);
+
+  pinMode(lidar_motor_speed, OUTPUT);
+  digitalWrite(lidar_motor_speed, LOW);
 
   led1.init();
   led1.on();
 
-  servoSetAll(0);
+  servoSetAll(400);
 
   delay(2000);
 
