d16a9e98c97a4eef613892d2cf0958fddb1d9e20
rmingon <ronanmingon69550@gmail.com>
2023-03-25 15:59:40 +0100

    add udp

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -3,31 +3,70 @@
 #include <Servo.h>
 #include <Led.h>
 #include <Lidar.h>
+#include "WiFi.h"
+#include "AsyncUDP.h"
+#include "Conf.h"
 
-Led led1(32);
-Led led2(33);
+Led led1 = Led(32);
+Led led2 = Led(33);
 
-Lidar lidar;
+Lidar lidar = Lidar();
+
+Servo servo = Servo();
 
 HardwareSerial SerialPort(2);
 
+AsyncUDP udp;
+
 void setup() {
   Serial.begin(115200);
-
   SerialPort.begin(128000, SERIAL_8N1, 16, 17);
+  led2.init();
+  led1.init();
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, PASS);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+      Serial.println("WiFi Failed");
+      while(1) {
+          delay(1000);
+      }
+  }
+  if(udp.listen(1234)) {
+      Serial.print("UDP Listening on IP: ");
+      Serial.println(WiFi.localIP());
+      udp.onPacket([](AsyncUDPPacket packet) {
+          Serial.print("UDP Packet Type: ");
+          Serial.print(packet.isBroadcast()?"Broadcast":packet.isMulticast()?"Multicast":"Unicast");
+          Serial.print(", From: ");
+          Serial.print(packet.remoteIP());
+          Serial.print(":");
+          Serial.print(packet.remotePort());
+          Serial.print(", To: ");
+          Serial.print(packet.localIP());
+          Serial.print(":");
+          Serial.print(packet.localPort());
+          Serial.print(", Length: ");
+          Serial.print(packet.length());
+          Serial.print(", Data: ");
+          Serial.write(packet.data(), packet.length());
+          Serial.println();
+          //reply to the client
+          packet.printf("Got %u bytes of data", packet.length());
+      });
+      led1.on();
+  }
+  led2.on();
 
-  servoInit();
+  servo.init();
 
   lidar.init();
 
-  led1.init();
-  led1.on();
-
-  servoSetAll(400);
+  servo.setAll(400);
 
   delay(2000);
 
-  servoSetAll(2048);
+  servo.setAll(2048);
 
   delay(10);
 
