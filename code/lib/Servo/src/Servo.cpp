830f67f4a3f96f4093bb03bcf7f0a4d1a60ff8a4
rmingon <ronanmingon69550@gmail.com>
2023-03-25 14:25:51 +0100

    set servo class

--- code/lib/Servo/src/Servo.cpp
+++ code/lib/Servo/src/Servo.cpp
@@ -1,12 +1,17 @@
 #include "Servo.h"
-#include <Adafruit_PWMServoDriver.h>
 
-Adafruit_PWMServoDriver servo_first = Adafruit_PWMServoDriver(0x41);
-Adafruit_PWMServoDriver servo_second = Adafruit_PWMServoDriver(0x40);
+#define SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
+#define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
+#define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
 
-#define SERVO_FREQ 50
+Servo::Servo() {
+    Adafruit_PWMServoDriver servo_first = Adafruit_PWMServoDriver(0x41);
+    Adafruit_PWMServoDriver servo_second = Adafruit_PWMServoDriver(0x40);
+};
 
-void servoInit() {
+void Servo::init() {
   servo_first.begin();
   servo_first.setOscillatorFrequency(27000000);
   servo_first.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
@@ -14,9 +19,9 @@ void servoInit() {
   servo_second.begin();
   servo_second.setOscillatorFrequency(27000000);
   servo_second.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
-}
+};
 
-void servoSetAll(uint16_t pwm) {
+void Servo::setAll(uint16_t pwm) {
   for(int i = 0; i < 16; i++) {
     servo_first.setPWM(i, 0, pwm);
   }
--- code/lib/Servo/src/Servo.h
+++ code/lib/Servo/src/Servo.h
@@ -2,6 +2,17 @@
 #define SERVO_H
 
 #include <Arduino.h>
+#include <Adafruit_PWMServoDriver.h>
+
+class Servo {
+  private:
+    Adafruit_PWMServoDriver servo_first;
+    Adafruit_PWMServoDriver servo_second;
+  public:
+    Servo();
+    void init();
+    void setAll(uint16_t pwm);
+};
 
 void servoInit();
 
